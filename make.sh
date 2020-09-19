5bb284433ed6ddc42f98550a11f09803492128e9
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-09-19 22:13:58 +0200

    new secrets

--- make.sh
+++ make.sh
@@ -1,12 +1,13 @@
 NAMESPACE=staging-1-3
 
 function create-secrets(){
-    kubectl create secret generic db-user-pass  --from-file=./private/password.txt
-    kubectl create secret generic odatests-tests-bot-password  --from-file=./private/testbot-password.txt
-    kubectl create secret generic odatests-secret-key  --from-file=./private/secret-key.txt
-    kubectl create secret generic minio-key  --from-file=./private/minio-key.txt
-    kubectl create secret generic jena-password  --from-file=./private/jena-password.txt
-    kubectl create secret generic logstash-entrypoint  --from-file=./private/logstash-entrypoint.txt
+    kubectl create secret -n $NAMESPACE generic db-user-pass  --from-file=./private/password.txt
+    kubectl create secret -n $NAMESPACE generic odatests-tests-bot-password  --from-file=./private/testbot-password.txt
+    kubectl create secret -n $NAMESPACE generic odatests-secret-key  --from-file=./private/secret-key.txt
+    kubectl create secret -n $NAMESPACE generic minio-key  --from-file=./private/minio-key.txt 
+    kubectl create secret -n $NAMESPACE generic jena-password  --from-file=./private/jena-password.txt
+    kubectl create secret -n $NAMESPACE generic logstash-entrypoint  --from-file=./private/logstash-entrypoint.txt
+    kubectl create secret -n $NAMESPACE generic gateway-secret  --from-file=gateway-secret.txt=$HOME/gateway-secret-hexified
 }
 
 function install() {
