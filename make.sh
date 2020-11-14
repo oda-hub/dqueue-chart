604b206ad53be8f5e0ac34682d8439e7751d7246
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2020-11-14 18:35:15 +0100

    new minio and resources

--- make.sh
+++ make.sh
@@ -4,6 +4,7 @@ function create-secrets(){
     kubectl create secret -n $NAMESPACE generic db-user-pass  --from-file=./private/password.txt
     kubectl create secret -n $NAMESPACE generic odatests-tests-bot-password  --from-file=./private/testbot-password.txt
     kubectl create secret -n $NAMESPACE generic odatests-secret-key  --from-file=./private/secret-key.txt
+    kubectl delete secret -n $NAMESPACE minio-key
     kubectl create secret -n $NAMESPACE generic minio-key  --from-file=./private/minio-key.txt 
     kubectl create secret -n $NAMESPACE generic jena-password  --from-file=./private/jena-password.txt
     kubectl create secret -n $NAMESPACE generic logstash-entrypoint  --from-file=./private/logstash-entrypoint.txt
@@ -20,7 +21,7 @@ function install() {
 
 function upgrade() {
     set -x
-    helm upgrade -n ${NAMESPACE:?} oda-dqueue . --set image.tag="$(cd dqueue; git describe --always)"
+    helm upgrade -n ${NAMESPACE:?} oda-dqueue . --set image.tag="$(cd dqueue; git describe --always)" --wait
 }
 
 $@
