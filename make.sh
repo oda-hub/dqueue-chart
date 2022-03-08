436e682eab5b0cd5921bb2ef0ebaf075205fe448
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2022-03-08 18:08:42 +0100

    adpatations

--- make.sh
+++ make.sh
@@ -6,8 +6,10 @@ function create-secrets(){
     kubectl create secret -n $ODA_NAMESPACE generic db-user-pass  --from-file=./private/password.txt
     kubectl create secret -n $ODA_NAMESPACE generic odatests-tests-bot-password  --from-file=./private/testbot-password.txt
     kubectl create secret -n $ODA_NAMESPACE generic odatests-secret-key  --from-file=./private/secret-key.txt
+
     kubectl delete secret -n $ODA_NAMESPACE minio-key
     kubectl create secret -n $ODA_NAMESPACE generic minio-key  --from-file=./private/minio-key.txt 
+
     kubectl create secret -n $ODA_NAMESPACE generic jena-password  --from-file=./private/jena-password.txt
     kubectl create secret -n $ODA_NAMESPACE generic logstash-entrypoint  --from-file=./private/logstash-entrypoint.txt
     kubectl create secret -n $ODA_NAMESPACE generic gateway-secret  --from-file=gateway-secret.txt=$HOME/gateway-secret-hexified
